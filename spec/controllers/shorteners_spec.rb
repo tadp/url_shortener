87ca35734dbdc64cc83f2beaca642b58d0360792
Tad Park <june@tutormentor.org>
2014-11-01 19:57:20 -0700

    adds more controller specs

--- spec/controllers/shorteners_spec.rb
+++ spec/controllers/shorteners_spec.rb
@@ -14,5 +14,19 @@ describe ShortenersController do
 			post :create, long_url: @shortener.long_url
 			expect(response).to redirect_to shortener_path(@shortener)
 	  end
+
+	  it "creates a new shortener if a shortener is not found" do
+			post :create, long_url: 'www.example.com'
+			expect(Shortener.count).to eq(1)
+	  end
 	end
+
+	describe "GET show" do
+		it "returns an instance of a shortener" do
+			@shortener = FactoryGirl.create(:shortener)
+			get :show, id: @shortener.id
+			expect(@shortener).to be_instance_of Shortener
+	  end
+	end
+
 end
